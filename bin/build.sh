beb84a4c067f2f56eafb62425656966c9e69415c
daffadon <daffaputranarendra9@gmail.com>
2025-07-14 15:40:59 +0700

    chore(ci): add ci script

--- bin/build.sh
+++ bin/build.sh
@@ -1,15 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 
-mkdir -p ./dist
+mkdir -p ./bin/dist
 
 service_name="user_service"
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "./dist/$service_name" ../cmd/
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "./bin/dist/$service_name" ./cmd
 wait
 
-VERSION=$(cat ../VERSION)
-echo "Building Docker image for $service_name:$VERSION" >&2
-docker build -t "$service_name:$VERSION" --build-arg BIN_NAME=$service_name -f ../Dockerfile ../
+VERSION=$(cat VERSION)
+echo "Building Docker image for $service_name:$VERSION" >/dev/stderr
+docker build -t "$service_name:$VERSION" --build-arg BIN_NAME=$service_name -f Dockerfile .
 
-echo "removing dist folder in local" >&2
-rm -r dist
+registry_image="$CI_REGISTRY_IMAGE:$VERSION"
+echo "$CI_JOB_TOKEN" | docker login -u gitlab-ci-token --password-stdin "$CI_REGISTRY"
+
+echo "Tagging image as $registry_image" >/dev/stderr
+docker tag "$service_name:$VERSION" "$registry_image"
+docker push "$registry_image"
+
+echo "Tagging image as $registry_image" >/dev/stderr
+docker tag "$service_name:$VERSION" "$CI_REGISTRY_IMAGE:latest"
+docker push "$CI_REGISTRY_IMAGE:latest"
+
+echo "Removing local image and tagged image" >/dev/stderr
+docker rmi "$service_name:$VERSION"
+docker rmi "$registry_image"
+docker rmi "$CI_REGISTRY_IMAGE:latest"
