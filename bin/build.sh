5767573005244520b844bfbca38a65350d21b971
daffadon <daffaputranarendra9@gmail.com>
2025-07-22 10:06:01 +0700

    chore(build): add optimization to the binary size

--- bin/build.sh
+++ bin/build.sh
@@ -4,7 +4,15 @@ mkdir -p ./bin/dist
 
 service_name="user_service"
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "./bin/dist/$service_name" ./cmd
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o "./bin/dist/$service_name" ./cmd
+wait
+
+if ! command -v upx >/dev/null 2>&1; then
+  echo "UPX not found. Installing..."
+  apk update && apk add upx
+fi
+
+upx --best --lzma ./bin/dist/$service_name
 wait
 
 VERSION=$(cat VERSION)
