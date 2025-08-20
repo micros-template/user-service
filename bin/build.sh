d19334b93a8affb475cce1a293a67160633f9df2
daffadon <daffaputranarendra9@gmail.com>
2025-08-20 08:38:26 +0700

    chore(script): fix build script

--- bin/build.sh
+++ bin/build.sh
@@ -4,6 +4,7 @@ set -e
 mkdir -p ./bin/dist
 
 service_name="user_service"
+full_image_name="docker-registry.anandadf.my.id/micros-template/$service_name"
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o "./bin/dist/$service_name" ./cmd
 wait
@@ -20,18 +21,18 @@ VERSION=$(cat VERSION)
 echo "Building Docker image for $service_name:$VERSION" >/dev/stderr
 docker build -t "$service_name:$VERSION" --build-arg BIN_NAME=$service_name -f Dockerfile .
 
-registry_image="$CI_REGISTRY_IMAGE:$VERSION"
+registry_image="$full_image_name:$VERSION"
 echo "${HARBOR_ROBOT_PASSWORD}" | docker login https://docker-registry.anandadf.my.id -u "${HARBOR_ROBOT_USERNAME}" --password-stdin
 
 echo "Tagging image as $registry_image" >/dev/stderr
 docker tag "$service_name:$VERSION" "$registry_image"
 docker push "$registry_image"
 
-echo "Tagging image as $registry_image" >/dev/stderr
-docker tag "$service_name:$VERSION" "$CI_REGISTRY_IMAGE:latest"
-docker push "$CI_REGISTRY_IMAGE:latest"
+echo "Tagging image as $full_image_name:latest" >/dev/stderr
+docker tag "$service_name:$VERSION" "$full_image_name:latest"
+docker push "$full_image_name:latest"
 
 echo "Removing local image and tagged image" >/dev/stderr
 docker rmi "$service_name:$VERSION"
 docker rmi "$registry_image"
-docker rmi "$CI_REGISTRY_IMAGE:latest"
+docker rmi "$full_image_name:latest"
--- bin/clean-test-image.sh
+++ bin/clean-test-image.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+
+service_name="user_service"
+full_image_name="docker-registry.anandadf.my.id/micros-template/$service_name"
+
 echo "Removing test in local" >/dev/stderr
-docker rmi "$CI_REGISTRY_IMAGE:test"
+docker rmi "$full_image_name:test"
